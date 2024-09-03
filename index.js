993c46c3d6fae4e1c1022d0c319ee2d40c46112d
Monika <laiskutis.monikai@gmail.com>
2024-09-03 09:46:15 +0300

    upd

--- index.js
+++ index.js
@@ -1,34 +1,26 @@
 import express from 'express';
-import { pageHome } from './pages/pageHome.js';
-import { page404 } from './pages/page404.js';
-import { pageAbout } from './pages/pageAbout.js';
-import { pageViewAllAccounts } from './pages/pageViewAllAccounts.js';
-import { pageCreateAccount } from './pages/pageCreateAccount.js';
+import { routerMain } from './router/routerMain.js';
 
 const app = express();
 const port = 5018;
 
+app.use(express.json({
+    type: 'application/json',
+}));
+app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    return res.send(pageHome());
-});
+// app.use((req, res, next) => {
+//     console.log('>>>', req.url);
+//     next();
+// }, (req, res, next) => {
+//     console.log(Date.now());
+//     next();
+// });
 
-app.get('/about', (req, res) => {
-    return res.send(pageAbout());
-});
+app.use('/', routerMain);
 
-app.get('/create-account', (req, res) => {
-    return res.send(pageCreateAccount());
-});
-
-app.get('/accounts', (req, res) => {
-    return res.send(pageViewAllAccounts());
-});
-
-app.get('*', (req, res) => {
-    return res.send(page404());
-});
 
 app.use((req, res, next) => {
     return res.status(404).send("Sorry can't find that!");
